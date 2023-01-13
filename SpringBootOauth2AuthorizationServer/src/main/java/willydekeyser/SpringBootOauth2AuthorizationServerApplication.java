47f8842472d41740cf2a178f3061927d99768072
willydekeyser <willy.de.keyser@skynet.be>
2023-01-13 22:43:35 +0100

    Setup Spring Boot Oauth2 Client Spring Boot Oauth2 Authorization Server Spring Boot Oauth2 Resource Server

---
+++ SpringBootOauth2AuthorizationServer/src/main/java/willydekeyser/SpringBootOauth2AuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauth2AuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOauth2AuthorizationServerApplication.class, args);
+	}
+
+}
---
+++ SpringBootOauth2ResourceServer/src/test/java/willydekeyser/SpringBootOauth2ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOauth2ResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
