538ae95b43c4c48fab22e3e883779fc456c08c64
willydekeyser <willy.de.keyser@skynet.be>
2023-01-16 16:12:49 +0100

    Update

---
+++ SpringBootOauth2ResourceServer/src/main/java/willydekeyser/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package willydekeyser.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	@Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+	String issuerUri;
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http
+				.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+				.oauth2ResourceServer(oauth2 -> oauth2
+						.jwt(jwt -> jwt.decoder(JwtDecoders.fromIssuerLocation(issuerUri))))
+				.build();
+	}
+}
---
+++ SpringBootOauth2ResourceServer/src/main/java/willydekeyser/controller/HomeController.java
@@ -0,0 +1,15 @@
+package willydekeyser.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home(Authentication authentication) {
+		return "Welcome Home! " + authentication.getName();
+	}
+
+}
