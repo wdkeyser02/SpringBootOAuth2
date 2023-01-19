8bba02a21c1000b7d0ce0625a869a52fe3a60df2
willydekeyser <ma074172@skynet.be>
2023-01-19 16:37:52 +0100

    Update LogFilter

---
+++ SpringBootOauth2ResourceServer/src/main/java/willydekeyser/config/LogFilter.java
@@ -0,0 +1,41 @@
+package willydekeyser.config;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Enumeration;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class LogFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		//HttpServletResponse httpResponse = (HttpServletResponse) response;
+		
+		LocalDateTime date = LocalDateTime.now();
+		System.err.println("LogFilter: " + date + " - " + httpRequest.getLocalAddr() + ":" + httpRequest.getLocalPort() + httpRequest.getServletPath());
+		Enumeration<String> headers = httpRequest.getHeaderNames();
+		while(headers.hasMoreElements()) {
+	        String headerName = (String)headers.nextElement();
+	        System.out.println("\tHeader: " + headerName + ":" + httpRequest.getHeader(headerName));
+	    }
+		System.out.println("\n\n");
+		chain.doFilter(request, response);
+		
+	}
+
+}
