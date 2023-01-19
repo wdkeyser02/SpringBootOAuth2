47a1b1e827478f7cd23537bcd9c359bec2b613e3
willydekeyser <ma074172@skynet.be>
2023-01-19 12:16:26 +0100

    Update

--- SpringBootOauth2Client/src/main/java/willydekeyser/SpringBootOauth2ClientApplication.java
+++ SpringBootOauth2Client/src/main/java/willydekeyser/SpringBootOauth2ClientApplication.java
@@ -2,8 +2,6 @@ package willydekeyser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringBootOauth2ClientApplication {
@@ -12,8 +10,4 @@ public class SpringBootOauth2ClientApplication {
 		SpringApplication.run(SpringBootOauth2ClientApplication.class, args);
 	}
 	
-	@Bean
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
-	}
 }
