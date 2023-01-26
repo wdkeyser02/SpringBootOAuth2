d5cd1ffe8b2e51d0a11736bfef15b8fe6cd67203
willydekeyser <ma074172@skynet.be>
2023-01-26 15:32:45 +0100

    Update OAuth2TokenCustomizer

--- SpringBootOauth2AuthorizationServer/src/main/java/willydekeyser/config/SecurityConfig.java
+++ SpringBootOauth2AuthorizationServer/src/main/java/willydekeyser/config/SecurityConfig.java
@@ -6,18 +6,23 @@ import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.core.oidc.endpoint.OidcParameterNames;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
@@ -26,6 +31,8 @@ import org.springframework.security.oauth2.server.authorization.config.annotatio
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
@@ -33,6 +40,8 @@ import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
+
+import willydekeyser.entity.User;
 
 @Configuration
 public class SecurityConfig {
@@ -62,15 +71,6 @@ public class SecurityConfig {
 				.build();
 		
 	}
-
-	//@Bean
-	//public UserDetailsService userDetailsService() {
-	//	var user1 = User.withUsername("user")
-	//			.password(passwordEncoder().encode("password"))
-	//			.authorities("read")
-	//			.build();
-	//	return new InMemoryUserDetailsManager(user1);
-	//}
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
@@ -105,6 +105,24 @@ public class SecurityConfig {
 		return ClientSettings.builder()
 				.requireProofKey(true)
 				.build();
+	}
+	
+	@Bean
+	OAuth2TokenCustomizer<JwtEncodingContext> tokenCustomizer() {
+		return context -> {
+			Authentication principal = context.getPrincipal();
+			if (context.getTokenType().getValue().equals("id_token")) {
+				context.getClaims().claim("Test", "Test Id Token");
+			}
+			if (context.getTokenType().getValue().equals("access_token")) {
+				context.getClaims().claim("Test", "Test Access Token");
+				Set<String> authorities = principal.getAuthorities().stream()
+                        .map(GrantedAuthority::getAuthority).collect(Collectors.toSet());
+                context.getClaims().claim("authorities", authorities)
+                        .claim("user", principal.getName());
+			}
+			
+		};
 	}
 	
 	@Bean 
