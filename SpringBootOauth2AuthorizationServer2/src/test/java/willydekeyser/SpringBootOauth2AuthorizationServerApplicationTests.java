e7d0184fd7c677c4fb7b72693962f654210ab842
willydekeyser <ma074172@skynet.be>
2023-01-31 17:08:40 +0100

    Setup Spring Authorization Server 2

---
+++ SpringBootOauth2AuthorizationServer2/src/test/java/willydekeyser/SpringBootOauth2AuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOauth2AuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
